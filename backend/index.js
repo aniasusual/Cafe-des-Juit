57f5e6275b4d922f36d0a2eb1330f614b195051e
aniasusual <82938725+aniasusual@users.noreply.github.com>
2023-07-10 22:22:56 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 4000
-const mongoDB = require("../db")
+const mongoDB = require("./db")
 mongoDB();
 
 app.use((req,res, next)=>{
@@ -18,9 +18,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
-app.use('/api', require("../routes/CreateUser"))
-app.use('/api', require("../routes/DisplayData"))
-app.use('/api', require("../routes/orderData"))
+app.use('/api', require("./routes/CreateUser"))
+app.use('/api', require("./routes/DisplayData"))
+app.use('/api', require("./routes/orderData"))
 
 
 app.listen(port, () => {
